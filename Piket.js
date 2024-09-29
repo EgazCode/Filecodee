818fb54e4c35c53b8753295e4b74a3ed2cb3ce1a
EgazCode <egasaputrament125@gmail.com>
2024-09-29 08:06:24 +0700

    Delete Piket.js

--- Piket.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const days = document.querySelectorAll(".day");
-    
-    days.forEach((day, index) => {
-        setTimeout(() => {
-            day.classList.add("show");
-        }, index * 300); // Delay antara setiap hari
-    });
-});
